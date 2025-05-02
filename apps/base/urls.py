e20777324736117bc42a169f0d9880b9e3b6d966
JesusD2405 <jesusdavid2405@gmail.com>
2025-05-02 00:27:11 -0400

    feature: adding view base for bank listing

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from ..auth2.views import loginView, LogoutView
 from ..client.views import ClientView
 from ..credit.views import CreditsView
+from ..bank.views import BankView
 
 urlpatterns = [
     # Default Path
@@ -9,9 +10,11 @@ urlpatterns = [
     # Auth 
     path('login', loginView, name="login"),
     path('logout', LogoutView, name='logout'),
-    # Clients
+    # Clientes
     path('clients', ClientView, name="clients_list"),
     # Creditos
     path('credits', CreditsView, name="credits_list"),
+    # Bancos
+    path('banks', BankView, name="banks_list"),
     
 ]
