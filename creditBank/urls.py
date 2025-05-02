6beed9587b6c44a17121271af5d5940eda74728f
JesusD2405 <jesusdavid2405@gmail.com>
2025-05-02 02:18:48 -0400

    feature: adding swagger base api documentation

--- creditBank/urls.py
+++ creditBank/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('', include('apps.base.urls')),
     path(settings.ADMIN_URL, admin.site.urls),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
